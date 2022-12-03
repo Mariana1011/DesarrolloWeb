b0e167e41c3d59c6b3b424fc32ff7a7a105f4743
unknown <mariana.ortega.mt@usa.edu.com>
2022-12-03 00:34:59 -0500

    primeras rutas

--- MiAppWeb/app.js
+++ MiAppWeb/app.js
@@ -3,8 +3,21 @@ import express from "express";
 const app = express();
 const puerto = 3000;
 
-function callback(){
+app.listen(puerto, () => {
     console.log("El servidor se esta ejecutando.");
-}
+})
 
-app.listen(puerto, callback)
+app.get("/", (req, res) =>{
+    //implementar el codigo
+    res.send("Hola")
+})
+
+app.get("/home", (req, res) =>{
+    //implementar el codigo
+    res.send("home a")
+})
+
+app.get("/home/g1", (req, res) =>{
+    //implementar el codigo
+    res.send("g1")
+})
