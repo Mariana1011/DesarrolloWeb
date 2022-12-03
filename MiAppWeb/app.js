4d5d7cd6e5409a463d80dd12a86ee245cec709a8
unknown <mariana.ortega.mt@usa.edu.com>
2022-12-03 00:16:51 -0500

    primer servidor

---
+++ MiAppWeb/app.js
@@ -0,0 +1,10 @@
+import express from "express";
+
+const app = express();
+const puerto = 3000;
+
+function callback(){
+    console.log("El servidor se esta ejecutando.");
+}
+
+app.listen(puerto, callback)
